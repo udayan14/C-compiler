aed3473bc369c51a93da1fc561f5965ec0a4a217
JOSHI UDAYAN PRAMOD <pramodj@cse.iitb.ac.in>
2018-02-14 15:39:30 +0530

    Added test script

--- Reference Implementation/Assignment2/commands.py
+++ Reference Implementation/Assignment2/commands.py
@@ -1,8 +1,11 @@
 import os
 import sys
 
-filename = str(sys.argv[1])
+os.system("list = $(find .  -name \"*.c\") ")	
 
-os.system("python3 150070001-150070018.py "+filename)
-os.system("./Assignment2 "+filename)
-os.system("diff Parser_ast_"+filename+".txt Parser_ast_"+filename+".txt1")
+os.system("for i in \"$list\"; do python3 150070001-150070018.py $i; ./Assignment2 $i; diff Parser_ast_{$i}.txt Parser_ast_{$i}.txt1 ")
+
+
+# os.system("python3 150070001-150070018.py "+filename)
+# os.system("./Assignment2 "+filename)
+# os.system("diff Parser_ast_"+filename+".txt Parser_ast_"+filename+".txt1")
---
+++ Reference Implementation/Assignment2/test.sh
@@ -0,0 +1,26 @@
+
+
+
+array=()
+find . -name "*.c" -print0 >tmpfile
+while IFS=  read -r -d $'\0'; do
+    array+=("$REPLY")
+done <tmpfile
+rm -f tmpfile
+
+len=${#array[*]}
+echo "found : ${len}"
+
+i=0
+
+while [ $i -lt $len ]
+do
+echo ${array[$i]}
+python3 150070001-150070018.py ${array[$i]};
+./Assignment2 ${array[$i]};
+ diff Parser_ast_${array[$i]}.txt Parser_ast_${array[$i]}.txt1 
+ echo "\n"
+let i++
+done
+
+
