d75571d6d097bb78c454a78f09dae1a06b48eb58
JOSHI UDAYAN PRAMOD <pramodj@cse.iitb.ac.in>
2018-02-14 16:41:47 +0530

    Test Script works perfect.

--- Reference Implementation/Assignment2/test.sh
+++ Reference Implementation/Assignment2/test.sh
@@ -1,27 +1,11 @@
-
-
-
-array=()
-find . -name "*.c" -print0 >tmpfile
-while IFS=  read -r -d $'\0'; do
-    array+=("$REPLY")
-done <tmpfile
-rm -f tmpfile
-
-len=${#array[*]}
-echo "found : ${len}"
-
-i=0
-
-while [ $i -lt $len ]
-do
-name = ${array[$i]} | tail -c +3
-echo ${array[$i]}
-python3 150070001-150070018.py ${name};
-./Assignment2 name;
- diff Parser_ast_${name}.txt Parser_ast_${name}.txt 
- echo "\n"
-let i++
+find . -name '*.c' | while read line; do
+	NAME="$(echo $line | tail -c +3)"
+	echo ${NAME}
+	 python3 150070001-150070018.py ${NAME};
+	./Assignment2 name;
+	 diff Parser_ast_${NAME}.txt Parser_ast_${NAME}.txt 
+	 echo "\n"   
 done
+
 
 
