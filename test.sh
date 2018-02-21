4174977d16460eca7031f46e72347ea6ec7a9c7b
JOSHI UDAYAN PRAMOD <pramodj@cse.iitb.ac.in>
2018-02-21 14:42:02 +0530

    Cleanind complete

---
+++ test.sh
@@ -0,0 +1,12 @@
+find . -name '*.c' | while read line; do
+	NAME="$(echo $line | tail -c +3)"
+	echo ${NAME}
+	 python3 150070001-150070018.py ${NAME};
+	./Assignment2 name;
+	 diff Parser_ast_${NAME}.txt Parser_ast_${NAME}.txt 
+	 echo "\n"   
+
+done
+
+
+
