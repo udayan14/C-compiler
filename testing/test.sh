62c0c4dd0997489ca5edab677d5d4006d9c691aa
ARCHIT GUPTA <archit@cse.iitb.ac.in>
2018-03-12 17:09:44 +0530

    empty if else allowed, while remaining ---file better.py

--- testing/test.sh
+++ testing/test.sh
@@ -1,9 +1,9 @@
 find . -name '*.c' | while read line; do
 	NAME="$(echo $line | tail -c +3)"
 	echo ${NAME}
-	 python3 150070001-150070018.py ${NAME};
+	 python3 better.py ${NAME};
 	./Parser ${NAME};
-	 diff -bB ${NAME}.cfg ${NAME}.cfg1 
+	 diff -bB ${NAME}.ast ${NAME}.ast1 
 	 echo   
 
 done
